a0c2c2727d83a769dd571e94ed1e8051bffa28de
dxenonb <drdxbme@gmail.com>
2022-12-09 17:15:52 -0500

    Add timer

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,24 @@
 import { russianTestSet } from "./modules/db.js";
 
-russianTestSet();
+class App {
+    constructor() {
+        this.timerStart = new Date();
+
+        const timerSeconds = 10 * 60;
+
+        setInterval(() => {
+            document.querySelector('#timer').textContent = renderTime(this.timerStart, timerSeconds);
+        }, 1000);
+    }
+}
+
+function renderTime(timerStart, timerSeconds) {
+    const endTime = timerStart.valueOf() + timerSeconds * 1000;
+    const rem = (endTime - new Date().valueOf()) / 1000;
+    const minutesRem = Math.floor(rem / 60);
+    const secondsRem = Math.floor(rem % 60);
+    return `${minutesRem}:${secondsRem}`;
+}
+
+await russianTestSet();
+new App();
